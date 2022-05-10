54a603ca5b5e7ee2914d059d26b00dfd99752662
Supakorn <supakorn2807@protonmail.com>
2022-05-10 18:56:51 +0700

    initial commit 🎉

--- src/routes/news.js
+++ src/routes/news.js
@@ -6,8 +6,8 @@ router.get("/", async (req, res) => {
   try {
     const { data } = await axios.get("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=63c1ed853d4c4142afe1195e9ff48276");
     res.render("news", { articles: data.articles });
-    console.log(data.articles);
   } catch (error) {
+    res.render("news", { articles: null });
     console.log(error);
   }
 });
